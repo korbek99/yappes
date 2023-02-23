511586341767a529eb8416e17f68547be72d21d6
JosePreatorianWise <preatorianwise99@hotmail.com>
2023-02-23 18:59:21 -0300

    add file plist backend

---
+++ YapeTest/Models/APIEndpointModel.swift
@@ -0,0 +1,13 @@
+//
+//  APIEndpointModel.swift
+//  YapeTest
+//
+//  Created by Jose David Bustos H on 23-02-23.
+//
+import Foundation
+
+public struct APIEndpointModel {
+    let url: URL
+    let APIKey: String
+    let APIToken: String?
+}
--- YapeTest/Services/webServicesMenu.swift
+++ YapeTest/Services/webServicesMenu.swift
@@ -8,10 +8,14 @@
 import Foundation
 import UIKit
 class webServicesMenu {
-    
+    var urlbase:String = ""
     func getArticles( completion: @escaping ([Product]?) -> ()) {
         
-        let url = URL(string: "http://demo3724643.mockable.io/")!
+        guard let endpointData = getEndpoint(fromName: "crearIssue") else { return }
+        
+        print(endpointData.url)
+        
+        let url = URL(string: endpointData.url.absoluteString)!
         
         URLSession.shared.dataTask(with: url) { [self] data, response, error in
             
@@ -36,6 +40,25 @@ class webServicesMenu {
         
     }
     
-
+    
+    public func getEndpoint(fromName: String) -> APIEndpointModel? {
+            var endpointFile = ""
+            #if DEBUG
+                endpointFile = "endpointsDev"
+            #else
+                endpointFile = "endpoints"
+            #endif
+            debugPrint(endpointFile)
+            guard let path = Bundle.main.path(forResource: endpointFile, ofType: "plist") else {
+                debugPrint("ERROR: No se encontró archivo endpoints.plist")
+                return nil
+            }
+            let myDict = NSDictionary(contentsOfFile: path) as! [String : Any]
+            guard let endpoint = myDict[fromName] as? [String : String] else {
+                debugPrint("ERROR: No existe endpoint con el nombre \(fromName)")
+                return nil
+            }
+            return APIEndpointModel(url: URL(string: endpoint["url"]!)!, APIKey: endpoint["x-api-key"]!, APIToken: endpoint["x-api-token"])
+        }
     
 }
